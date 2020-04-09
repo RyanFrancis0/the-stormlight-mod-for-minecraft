d92f83f80038fb43e317172852c8fb2a74c9e3cd
Ryan <0ryanfrancis@gmail.com>
2020-04-10 02:29:09 +1000

    some textures, gotta figure out custom renderer

---
+++ src/main/java/com/curse2014/stormlightmod/init/MyRender.java
@@ -0,0 +1,17 @@
+package com.curse2014.stormlightmod.init;
+
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.client.renderer.model.ModelManager;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.entity.IRendersAsItem;
+import net.minecraft.item.ItemStack;
+
+public class MyRender extends ItemRenderer {
+    public MyRender(TextureManager p_i46552_1_, ModelManager p_i46552_2_, ItemColors p_i46552_3_) {
+        super(p_i46552_1_, p_i46552_2_, p_i46552_3_);
+    }
+
+    @Override
+    
+}
